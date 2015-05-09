c24983ecd76747ebcaad0a881c181847e9041c06
Andrew Schwartz <andrew@3playmedia.com>
2015-05-09 15:38:46 -0400

    javascripting up the math

---
+++ app/assets/javascripts/modeling.js
@@ -0,0 +1,20 @@
+// take matrix data, truth, produce matrix of linear model parameter estimates
+function llmse(data, truth) {
+    if (data.size()[0] != truth.size()[0]){
+    	throw "training data size mismatch";
+    }
+    s = math.inv(math.multiply(data.transpose(), data));
+    r = math.multiply(s, data.transpse());
+    p = math.multpliy(r, truth);
+    return p;
+}
+
+// take array xx and produce matrix with powers of x in cols
+function nlFeatures(xx, powers) {
+	if (typeof(powers) == 'undefined') powers = [0,1];
+	mm = xx.length;
+	pp = powers.map(function(e) {
+		return (e==0 ? math.ones(mm)._data : math.dotPow(xx,e))
+	})
+	return math.matrix(pp).transpose()
+}
