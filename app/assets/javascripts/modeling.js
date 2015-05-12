e0d8b91afa8ad8407de99a6041278b76e010d36b
Andrew Schwartz <andrew@3playmedia.com>
2015-05-12 09:53:58 -0400

    deal with insufficient data, add clear button

--- app/assets/javascripts/modeling.js
+++ app/assets/javascripts/modeling.js
@@ -3,8 +3,12 @@ function llmse(data, truth) {
     if (data.size()[0] != truth.size()[0]){
     	throw "training data size mismatch";
     }
-    s = math.inv(math.multiply(data.transpose(), data));
-    r = math.multiply(s, data.transpose());
+    s = math.multiply(data.transpose(), data);
+    if (math.det(s) < 1e-10) {
+    	return math.multiply(math.ones(data.size()[1], 1), NaN)
+    }
+    si = math.inv(s);
+    r = math.multiply(si, data.transpose());
     p = math.multiply(r, truth);
     return p;
 }
@@ -12,6 +16,7 @@ function llmse(data, truth) {
 // take array xx and produce matrix with powers of x in cols
 function nlFeatures(xx, powers) {
 	if (typeof(powers) == 'undefined') powers = [0,1,2];
+	if (xx.length == 0) return math.ones(0,powers.length)
 	mm = xx.length;
 	pp = powers.map(function(e) {
 		return (e==0 ? math.ones(mm)._data : math.dotPow(xx,e))
