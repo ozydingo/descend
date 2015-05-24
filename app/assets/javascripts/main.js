6c34b5cdf223aed3b8b5d8888a19b884ff079af4
Andrew Schwartz <andrew@3playmedia.com>
2015-05-23 21:19:48 -0400

    huge reorg of data structures

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,6 @@
-//TODO: get rid of globals
-var thePlot, xyData = [], fitData = {}
-var descent;
+//TODO: get rid of globals?
+var xyData, xyFit, xPowers = [0,1];
+var thePlot, descent;
 
 function toggleDescent() {
 	var descent = $("#btn_descend").attr("value");
@@ -18,14 +18,14 @@ function toggleDescent() {
 // generate callback for "the data has changed"
 function updateData() {
 	xyData = pruneData(xyData);
-	fitData.llmse = computeFit(xyData);
-	plotIt(xyData, fitData);
+	xyFit = computeFit(xyData);
+	plotIt();
 }
 
 function descend(n) {
-	data = matrixData(xyData);
-	fitData.descent = descent.step(data.features, data.outcome, n);
-	plotIt(xyData, fitData);
+	data = modeling.xy.trainingData(xyData);
+	descent.step(data.features, data.outcomes, n);
+	plotIt();
 }
 
 function clearData() {
@@ -34,10 +34,7 @@ function clearData() {
 }
 
 // general function to plot data and any fit lines
-function plotIt(xyData, fits) {
-	var coefs, fitData
-	if (fits === undefined) fits = {};
-
+function plotIt() {
 	// define first plot data: the data points themselves
 	var series = [{
 		data: xyData,
@@ -53,8 +50,8 @@ function plotIt(xyData, fits) {
 	};
 
 	// add fit line if specified
-	if (fits.llmse) {
-		fitData = getFitPlotData(fits.llmse);
+	if (xyFit) {
+		var fitData = getFitPlotData(xyFit);
 		series.push({
 			data: fitData,
 			lines: {show: true},
@@ -63,8 +60,8 @@ function plotIt(xyData, fits) {
 	}
 
 	// add descent fit line if specified
-	if (fits.descent) {
-		fitData = getFitPlotData(fits.descent);
+	if (dFit = descent.getCoefs()) {
+		var fitData = getFitPlotData(dFit);
 		series.push({
 			data: fitData,
 			lines: {show: true},
@@ -84,30 +81,20 @@ function pruneData(xyData) {
 
 // return x,y data for llmse fit
 function getFitPlotData(coefs) {
-	if (coefs.size()[1]==0) return []
+	if (!coefs || coefs.size()[1]==0) return []
 	var xMin = thePlot.getAxes().xaxis.min;
 	var xMax = thePlot.getAxes().xaxis.max;
 	var xStep = (xMax - xMin) / 50;
 	var xVals = math.range(xMin, xMax + xStep, xStep)._data;
-	var fMatrix = modeling.nlFeatures(xVals);
-	var pMatrix = math.multiply(fMatrix, coefs);
-	var yVals = pMatrix.transpose()._data[0];
-	var fitData = math.matrix([xVals, yVals]).transpose()._data;
+	var yVals = modeling.xy.predict(coefs, xVals, xPowers);
+	var fitData = modeling.xy.zipXY(xVals, yVals)
 	return fitData;
-}
-
-function matrixData(xyData) {
-	var xPoints = xyData.map(function(xy){return xy[0]});
-	var yPoints = xyData.map(function(xy){return xy[1]});
-	var xData = modeling.nlFeatures(xPoints);
-	var yData = yPoints.length == 0 ? math.matrix() : math.matrix([yPoints]).transpose();
-	return {features: xData, outcome: yData}
 }
 
 // compute fit coeffs for llmse given data
 function computeFit(xyData) {
-	var data = matrixData(xyData);
-	var coefs = modeling.llmse(data.features, data.outcome);
+	var data = modeling.xy.trainingData(xyData)
+	var coefs = modeling.llmse(data.features, data.outcomes);
 	return coefs;    
 }
 
@@ -116,9 +103,9 @@ $(document).ready( function() {
 	$("#btn_descend").click(toggleDescent);
 	$("#var_maxN").change(updateData);
 
-	thePlot = plotIt([]);
-	descent = modeling.descend();
-
+	descent = modeling.descent();
+	clearData();
+	thePlot = plotIt();
 
 	$("#theGraph").bind("plotclick", function(event, pos, item){
 		xyData.push([pos.x, pos.y]);
--- app/assets/javascripts/modeling.js
+++ app/assets/javascripts/modeling.js
@@ -15,7 +15,7 @@ modeling = function() {
 	}
 
 	// create new instance of descend model
-	function descend() {
+	function descent() {
 		var coefs, alpha = 0.05;
 
 		function step(features, outcome, n) {
@@ -48,20 +48,81 @@ modeling = function() {
 		}
 	};
 
-	// take array xx and produce matrix with powers of x in cols
-	function nlFeatures(xx, powers) {
-		if (powers === undefined) powers = [0,1,2,3];
-		if (xx.length == 0) return math.ones(0,powers.length)
-		mm = xx.length;
-		pp = powers.map(function(e) {
-			return (e==0 ? math.ones(mm)._data : math.dotPow(xx,e))
-		})
-		return math.matrix(pp).transpose()
-	}
+	// helper for data tasks, e.g.
+	// transcforming x, y vectors into features / outcome matrices
+	// generating x or x&y support vectors
+	var xy = function() {
+		// generate a matrix of input features from array of x values
+		function features(xArray, powers) {
+			if (powers==undefined) powers = [0,1];
+			if (xArray.length==0) return math.ones(0, powers.length);
+			var pp = powers.map(function(ee) {
+				if (ee==0) {
+					return math.ones(xArray.length)
+				} else if (ee==1) {
+					return math.matrix(xArray)
+				} else {
+					return math.dotPow(xArray,ee)
+				}
+			})
+			return math.matrix(pp).transpose()
+		}
+
+		// generate a matrix of outcomes from array of y values
+		function outcomes(yArray) {
+			return yArray.length == 0 ? math.matrix() : math.matrix([yArray]).transpose();
+		}
+
+		// return features and outcomes from zipped xy Arrays
+		function trainingData(zippedXY) {
+			var unzippedXY = unzipXY(zippedXY);
+			var f = features(unzippedXY.x);
+			var o = outcomes(unzippedXY.y);
+			return {features: f, outcomes: o}
+		}
+
+		// generate matrix of support values for given dimensions
+		function basis() {
+			throw "basis() is NIY"
+		}
+
+		// predict y (Array) from x (Array) given coefs (Matrix) and powers (Array)
+		function predict(coefs, xArray, powers) {
+			if (powers===undefined) powers=[0,1];
+			if (powers.length != coefs.size()[0]) throw "Size mismatch between powers and coefs";
+			var fMatrix = features(xArray, powers);
+			var pMatrix = math.multiply(fMatrix, coefs);
+			return pMatrix.transpose()._data[0];
+		}
+
+		// zip arrays x and y into [x,y] paris
+		function zipXY(xArray, yArray) {
+			return xArray.map(function(_,ii) {
+				return [xArray[ii], yArray[ii]];
+			});
+		}
+
+		// unzip Array of [x,y] into xArray and yArray
+		function unzipXY(xyArray) {
+			var xArray = xyArray.map(function(xy){return xy[0]});
+			var yArray = xyArray.map(function(xy){return xy[1]});
+			return {x: xArray, y: yArray}
+		}
+
+		return {
+			features: features,
+			outcomes: outcomes,
+			trainingData: trainingData,
+			predict: predict,
+			basis: basis,
+			zipXY: zipXY,
+			unzipXY: unzipXY
+		}
+	}();
 
 	return {
 		llmse: llmse,
-		nlFeatures: nlFeatures,
-		descend: descend
+		descent: descent, 
+		xy: xy
 	}
 }();
