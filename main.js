da3eae3c3fda9af4fb86c263e58e2440b9fd0fc0
Andrew Schwartz <andrew@3playmedia.com>
2016-11-05 15:19:27 -0700

    feat(costLog) [AS] Add cost log graph

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ var mainGraph;
 $(document).ready( function() {
 	mainGraph = dataGraph("main");
 	mainGraph.linkHUD(costscape3d($("#costscape"), mainGraph))
+	mainGraph.linkHUD(costLog($("#costLog"), mainGraph))
 } );
